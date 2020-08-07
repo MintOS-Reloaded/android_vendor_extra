3aebb83409a3ab92b2650a0c63bee50ebb5cbbed
nift4 <nift4@protonmail.com>
2020-08-07 19:01:13 +0200

    Update product.mk for Mint OS

--- product.mk
+++ product.mk
@@ -2,26 +2,12 @@
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.opa.eligible_device=true
 
-ifeq ($(SIGNED_BUILD),true)
-# Use own keys
-PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/certs/releasekey
-# Use := instead of += to remove Lineage's keys
-PRODUCT_EXTRA_RECOVERY_KEYS := vendor/certs/releasekey
-endif
-
 ifeq ($(WITH_GAPPS),true)
 
-# mindthegapps
-$(call inherit-product, vendor/gapps/common/common-vendor.mk)
+GAPPS_VARIANT := nano
+$(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
 
 endif
-
-# Recovery ADB keys
-PRODUCT_COPY_FILES += \
-    vendor/extra/adb_keys:recovery/root/adb_keys
 
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay
-
-# Use own updater
-PRODUCT_PROPERTY_OVERRIDES += lineage.updater.uri=https://lineage.stricted.net/api/v1/{device}/{type}/{incr}
