15df860a968489e32163eb95f641002ccd8c55dc
nift4 <nift4@protonmail.com>
2020-12-16 14:26:37 +0100

    Update product.mk

--- product.mk
+++ product.mk
@@ -2,7 +2,7 @@
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.opa.eligible_device=true
 
-ifeq ($(WITH_GAPPS),true)
+ifeq (${WITH_GAPPS},true)
 
 GAPPS_VARIANT := nano
 $(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
@@ -13,4 +13,4 @@ endif
 PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay
 PRODUCT_PROPERTY_OVERRIDES += lineage.updater.uri=https://github.com/nift4/Mint-OS/raw/ota/{device}.json
 
-$(call inherit-product, vendor/extra/${device}.mk)
+$(call inherit-product-if-exists, vendor/extra/${device}.mk)
