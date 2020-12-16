5082e7706a36c96b6161c4aed924f75076fface6
nift4 <nift4@protonmail.com>
2020-12-16 17:04:53 +0100

    Create montana.mk

---
+++ montana.mk
@@ -0,0 +1,12 @@
+# ARCore
+TARGET_INCLUDE_STOCK_ARCORE := true
+
+PRODUCT_COPY_FILES += \
+    vendor/extra/android.hardware.camera.ar.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.ar.xml
+
+# Face Unlock
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face.moto_unlock_service=true
