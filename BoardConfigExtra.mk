035200087cd621281bd96c7c185011ab9002a8da
nift4 <nift4@protonmail.com>
2020-12-17 11:54:06 +0100

    Update BoardConfigExtra.mk

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -1,5 +1,7 @@
 BOARD_SEPOLICY_DIRS += vendor/extra/sepolicy
 
+PRODUCT_PACKAGES += //vendor/extra:LineageFpg
+
 # Init
 TARGET_INIT_VENDOR_LIB := //vendor/extra:libinit_mint
 TARGET_RECOVERY_DEVICE_MODULES := //vendor/extra:libinit_mint
