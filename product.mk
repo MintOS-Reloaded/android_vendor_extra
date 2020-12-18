bebe09d5434b8d55270dc3ab39b233fb950a8154
nift4 <nift4@protonmail.com>
2020-12-18 10:41:17 +0100

    Update product.mk

--- product.mk
+++ product.mk
@@ -2,15 +2,74 @@
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.opa.eligible_device=true
 
-ifeq (${WITH_GAPPS},true)
-
-GAPPS_VARIANT := nano
-$(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
-
-endif
-
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay
-PRODUCT_PROPERTY_OVERRIDES += lineage.updater.uri=https://github.com/nift4/Mint-OS/raw/ota/{device}.json
 
 $(call inherit-product-if-exists, vendor/extra/${device}.mk)
+
+ifeq (${WITH_GAPPS},true)
+ifeq ($(TARGET_GAPPS_ARCH),)
+$(error "GAPPS: TARGET_GAPPS_ARCH is undefined")
+endif
+
+ifneq ($(TARGET_GAPPS_ARCH),arm)
+ifneq ($(TARGET_GAPPS_ARCH),arm64)
+$(error "GAPPS: Only arm and arm64 are allowed")
+endif
+endif
+
+PRODUCT_PACKAGES += \
+    com.google.android.maps
+
+ifeq ($(IS_PHONE),true)
+PRODUCT_PACKAGES += \
+    com.google.android.dialer.support
+endif
+
+$(call inherit-product, vendor/gapps/common-blobs.mk)
+
+ifeq ($(TARGET_INCLUDE_STOCK_ARCORE),true)
+PRODUCT_PACKAGES += \
+    arcore
+endif
+
+# System app
+PRODUCT_PACKAGES += \
+    GoogleExtShared \
+    GooglePrintRecommendationService
+
+# System priv-app
+PRODUCT_PACKAGES += \
+    GoogleExtServicesPrebuilt
+
+# Product app
+PRODUCT_PACKAGES += \
+    GoogleContactsSyncAdapter \
+    GoogleTTS \
+    MarkupGoogle \
+    SoundPickerPrebuilt \
+    talkback \
+    WebViewGoogle
+    
+# Product priv-app
+PRODUCT_PACKAGES += \
+    AndroidMigratePrebuilt \
+    CarrierServices \
+    ConfigUpdater \
+    ConnMetrics \
+    GoogleOneTimeInitializer \
+    GooglePartnerSetup \
+    GoogleServicesFramework \
+    Phonesky \
+    PrebuiltGmsCoreQt \
+    SetupWizardPrebuilt \
+    TurboPrebuilt \
+    Velvet \
+    WallpaperPickerGoogleRelease \
+    WellbeingPrebuilt
+
+
+PRODUCT_PROPERTY_OVERRIDES += lineage.updater.uri=https://github.com/nift4/Mint-OS/raw/ota/gapps_{device}.json
+else
+PRODUCT_PROPERTY_OVERRIDES += lineage.updater.uri=https://github.com/nift4/Mint-OS/raw/ota/{device}.json
+endif
