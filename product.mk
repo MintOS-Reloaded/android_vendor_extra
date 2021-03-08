cc819e328104863419436dec6057b70c861c11f5
Nick (nift4) <nift4@protonmail.com>
2021-03-08 13:37:04 +0100

    Support Face Unlock

--- product.mk
+++ product.mk
@@ -7,6 +7,16 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay
 
 include vendor/extra/${device}.mk
 
+TARGET_FACE_UNLOCK_SUPPORTED ?= true
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
+
 ifeq (${WITH_GAPPS},true)
 ifeq ($(TARGET_GAPPS_ARCH),)
 $(error "GAPPS: TARGET_GAPPS_ARCH is undefined")
