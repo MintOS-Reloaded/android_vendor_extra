64dc551e4a7c34d7e582321362346c5929512817
Nick (nift4) <nift4@protonmail.com>
2021-03-08 15:54:46 +0100

    Swipelibs for everyone [1/2]

--- cedric.mk
+++ cedric.mk
@@ -6,7 +6,3 @@ PRODUCT_COPY_FILES += \
     
 # GApps
 TARGET_GAPPS_ARCH := arm64
-
-# Swipelibs
-PRODUCT_COPY_FILES += \
-    vendor/extra/libjni_latinimegoogle.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libjni_latinimegoogle.so
