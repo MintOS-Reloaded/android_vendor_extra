1240118bda7df91d531ee03cfcf9dbbf033605ab
Nick (nift4) <nift4@protonmail.com>
2021-03-08 13:38:30 +0100

    Update pro1.mk

--- pro1.mk
+++ pro1.mk
@@ -1,10 +1,3 @@
-# Face Unlock
-PRODUCT_PACKAGES += \
-    FaceUnlockService
-
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.face.moto_unlock_service=true
-
 # GApps
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_STOCK_ARCORE := true
